0228302a907e123e4946aa30e68adf90fe42ec3d
baithak-starmark <amit.singh.kaizen2018@gmail.com>
2019-11-15 21:37:10 +0530

    demo

---
+++ demo-git-1/src/main/java/guru/baithak/demo_git_1/App.java
@@ -0,0 +1,13 @@
+package guru.baithak.demo_git_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
